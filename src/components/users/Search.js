3c38e0bc802cd70ae1c051d7ab67dd3227e01b0c
meghrajfalle <meghraj.falle@appdirect.com>
2020-08-16 10:20:51 +0530

    Branch11: Added Alert and passed it to App.js and created setAlert function with timeout. Also added Alert.js to render it in App.js

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -9,15 +9,20 @@ class Search extends Component {
   static propTypes = {
     searchUsers: PropTypes.func.isRequired,
     clearUsers: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired
   };
 
   onChange = e => this.setState({ [e.target.name]: e.target.value });
 
   onSubmit = e => {
     e.preventDefault();
-    this.props.searchUsers(this.state.text);
-    this.setState({ text: '' });
+    if (this.state.text === '') {
+      this.props.setAlert('Please enter some text', 'light');
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: '' });
+    }
   };
 
   render() {
