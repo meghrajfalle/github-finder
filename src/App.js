4f08736dbdde337d9a7084bd657b6a76d5c726bc
meghrajfalle <meghraj.falle@appdirect.com>
2020-08-17 02:31:00 +0530

    Branch24: Added Home.js and NotFound.js and added their route in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NavBar from './components/layout/Navbar';
-import Users from './components/users/Users';
-import Search from './components/users/Search';
+import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 import User from './components/users/User';
@@ -22,18 +22,10 @@ const App = () => {
             <div className='container'>
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path='/'
-                  render={props => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/users/:login' component={User} />} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
