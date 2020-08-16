924c8fe9bd4d0ca0cb889e684c31393fd1da805c
meghrajfalle <meghraj.falle@appdirect.com>
2020-08-17 02:12:26 +0530

    Branch23: Adding a new Alert context with 3 files - alertContext.js ,AlerState.js and alertReducer.js and removing the Alert state from App.js and props and also refactoring the Alert.js by importing alertContext in it. Also removed the proptypes from Search.js and imported AlertContext.js so that alertContext.setAlert function can be called from there

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NavBar from './components/layout/Navbar';
 import Users from './components/users/Users';
@@ -10,40 +10,35 @@ import './App.css';
 import '../.env.local';
 
 import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
 
 const App = () => {
-  const [alert, setAlert] = useState(null);
-
-  //Set alert
-  const showAlert = (msg, type) => {
-    setAlert({ msg, type });
-    setTimeout(() => this.setState({ alert: null }), 5000);
-  };
-
   return (
     <GithubState>
-      <Router>
-        <div className='App'>
-          <NavBar title='Github Finder' icon='fab fa-github' />
-          <div className='container'>
-            <Alert alert={alert} />
-            <Switch>
-              <Route
-                exact
-                path='/'
-                render={props => (
-                  <Fragment>
-                    <Search setAlert={showAlert} />
-                    <Users />
-                  </Fragment>
-                )}
-              />
-              <Route exact path='/about' component={About} />
-              <Route exact path='/users/:login' component={User} />} />
-            </Switch>
+      <AlertState>
+        <Router>
+          <div className='App'>
+            <NavBar title='Github Finder' icon='fab fa-github' />
+            <div className='container'>
+              <Alert />
+              <Switch>
+                <Route
+                  exact
+                  path='/'
+                  render={props => (
+                    <Fragment>
+                      <Search />
+                      <Users />
+                    </Fragment>
+                  )}
+                />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/users/:login' component={User} />} />
+              </Switch>
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
+      </AlertState>
     </GithubState>
   );
 };
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,18 +1,19 @@
 import React, { useState, useContext } from 'react';
-import PropTypes from 'prop-types';
 import GithubContext from '../../context/github/githubContext';
+import AlertContext from '../../context/alert/alertContext';
 
-const Search = ({ setAlert }) => {
+const Search = () => {
   const githubContext = useContext(GithubContext);
+  const alertContext = useContext(AlertContext);
 
-  const [text, setText] = useState(' ');
+  const [text, setText] = useState('');
 
   const onChange = e => setText(e.target.value);
 
   const onSubmit = e => {
     e.preventDefault();
     if (text === '') {
-      setAlert('Please enter some text', 'light');
+      alertContext.setAlert('Please enter some text', 'light');
     } else {
       githubContext.searchUsers(text);
       setText('');
@@ -45,10 +46,6 @@ const Search = ({ setAlert }) => {
       )}
     </div>
   );
-};
-
-Search.propTypes = {
-  setAlert: PropTypes.func.isRequired
 };
 
 export default Search;
