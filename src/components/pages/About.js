347065e96325e58365c0896c5c2ebeba81887e44
meghrajfalle <meghraj.falle@appdirect.com>
2020-10-11 12:23:32 +0530

    Branch26: Added Favicon and made some space between icon and title in nav. Also Changed the About us page with better look

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,11 +3,82 @@ import React, { Fragment } from 'react';
 const About = () => {
   return (
     <Fragment>
-      <h1>About this App</h1>
-      <p>Purpose: App to search github users</p>
-      <p>Version: 1.0.0</p>
-      <p>Created by: Meghraj Falle </p>
-      <a href='https://www.linkedin.com/in/meghrajfalle/'> Linkedin Profile </a>
+      <h1>About this App and Developer</h1>
+      <table>
+        <tr>
+          <td>
+            <p>
+              <strong>Purpose:</strong>
+            </p>
+          </td>
+          <td>
+            This webapp is to search github users and their profile details
+            along with their public repostories. Created using ReactJs and makes
+            use of github's public API's
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <p>
+              <strong>Version:</strong>
+            </p>
+          </td>
+          <td>1.0.0</td>
+        </tr>
+        <tr>
+          <td>
+            <p>
+              <strong>Developed By:</strong>
+            </p>
+          </td>
+          <td>Meghraj Falle</td>
+        </tr>
+        <tr>
+          <td>
+            <strong>Links for other projects by Meghraj Falle:</strong>
+          </td>
+          <td>
+            <a
+              href='https://meghraj-portfolio.herokuapp.com/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <u> Meghraj Falle Portfolio site</u>
+            </a>
+            <br />
+            <a
+              href='https://contact-keeper-megh.herokuapp.com/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <u>Contact Keeper | Keep Track of your Contact</u> 
+            </a>
+            <br />
+            <a
+              href='https://projectmanagement-app.herokuapp.com/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <u>Project Management Tool</u> 
+            </a>
+            <br />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <strong>Connect with me:</strong>
+          </td>
+          <td>
+            <a
+              href='https://www.linkedin.com/in/meghrajfalle/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <u>Linkedin Profile </u>
+            </a>
+          </td>
+        </tr>
+      </table>
     </Fragment>
   );
 };
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,26 @@
-import React, { Fragment } from 'react';
+import React, { useContext, Fragment } from 'react';
 import Search from '../users/Search';
 import Users from '../users/Users';
+import GithubContext from '../../context/github/githubContext';
 
 const Home = () => {
+  const githubContext = useContext(GithubContext);
+  const { loading, users } = githubContext;
+
+  if (!loading && users.length == 0) {
+    return (
+      <Fragment>
+        <Search />
+        <Users />
+        <br />
+        <h3 style={{ textAlign: 'center' }}>
+          Search for the github user that you would love to know about and
+          checkout his profile and repository details!{' '}
+        </h3>
+      </Fragment>
+    );
+  }
+
   return (
     <Fragment>
       <Search />
